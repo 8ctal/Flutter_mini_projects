f245956581253ea6d644ced3560198a0066a4c11
8ctal <camilojaimes04@gmail.com>
2025-02-26 11:41:58 -0500

    Adding stateRouter to the bottomNavBar

--- movies_app/lib/config/router/app_router.dart
+++ movies_app/lib/config/router/app_router.dart
@@ -5,9 +5,13 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-        path: '/',
+        path: '/home/:page',
         name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
+        builder: (context, state) {
+          final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+
+          return HomeScreen(pageIndex: pageIndex);
+        },
         routes: [
           GoRoute(
             path: 'movie/:id',
@@ -19,5 +23,59 @@ final appRouter = GoRouter(
             },
           ),
         ]),
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
+    ),
   ],
 );
+
+//* This is the way to use the ShellRoute without preserving the state
+    // ShellRoute(
+    //     builder: (context, state, child) {
+    //       return HomeScreen(childView: child);
+    //     },
+    //     routes: [
+    //       GoRoute(
+    //           path: '/',
+    //           builder: (context, state) {
+    //             return const HomeView();
+    //           },
+    //           routes: [
+    //             GoRoute(
+    //               path: 'movie/:id',
+    //               name: MovieScreen.name,
+    //               builder: (context, state) {
+    //                 final movieId = state.pathParameters['id'] ?? 'no-id';
+
+    //                 return MovieScreen(movieId: movieId);
+    //               },
+    //             ),
+    //           ]),
+    //       GoRoute(
+    //         path: '/favorites',
+    //         builder: (context, state) {
+    //           return const FavoritesView();
+    //         },
+    //       ),
+    //     ]),
+ //* Base case
+//     GoRoute(
+//         path: '/',
+//         name: HomeScreen.name,
+//         builder: (context, state) => const HomeScreen(
+//               childView: HomeView(),
+//             ),
+//         routes: [
+//           GoRoute(
+//             path: 'movie/:id',
+//             name: MovieScreen.name,
+//             builder: (context, state) {
+//               final movieId = state.pathParameters['id'] ?? 'no-id';
+
+//               return MovieScreen(movieId: movieId);
+//             },
+//           ),
+//         ]),
+//   ],
+// );
